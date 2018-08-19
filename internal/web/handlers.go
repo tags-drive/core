4308bd3cd5e2353aa3fd67c43adc13aa3394fbfb
ShoshinNikita <shoshin.nikita@gmail.com>
2018-08-19 15:42:14 +0300

    Add index.html

--- internal/web/handlers.go
+++ internal/web/handlers.go
@@ -1,12 +1,17 @@
 package web
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +24,6 @@ func auth(w http.ResponseWriter, r *http.Request) {
 
 func checkAuth(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		h.ServeHTTP(w, r)
 	})
 }
