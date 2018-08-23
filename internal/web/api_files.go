9cbf03297380b91074990e71a521f536f2262007
ShoshinNikita <shoshin.nikita@gmail.com>
2018-08-24 01:07:24 +0300

    Change order of functions in api_files.go
    GET -> POST -> PUT -> DELETE

--- internal/web/api_files.go
+++ internal/web/api_files.go
@@ -30,141 +30,6 @@ type multiplyResponse struct {
 	Status   string `json:"status"` // Status isn't empty when IsError == false
 }
 
-/* Files */
-
-// POST /api/files (multipart/form-data)
-//
-// Response: json list of strings with status of files uploading
-//
-func upload(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(maxSize)
-	if err != nil {
-		switch err {
-		case http.ErrNotMultipart:
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		default:
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		return
-	}
-
-	respChan := make(chan multiplyResponse, 50)
-	wg := new(sync.WaitGroup)
-
-	for _, f := range r.MultipartForm.File["files"] {
-		wg.Add(1)
-		go func(header *multipart.FileHeader) {
-			defer wg.Done()
-			err := files.UploadFile(header)
-			if err != nil {
-				respChan <- multiplyResponse{Filename: header.Filename, IsError: true, Error: err.Error()}
-			} else {
-				respChan <- multiplyResponse{Filename: header.Filename, Status: "uploaded"}
-			}
-		}(f)
-	}
-	wg.Wait()
-	close(respChan)
-
-	var response []multiplyResponse
-	for resp := range respChan {
-		response = append(response, resp)
-	}
-
-	enc := json.NewEncoder(w)
-	if params.Debug {
-		enc.SetIndent("", "  ")
-	}
-	enc.Encode(response)
-}
-
-// DELETE /api/files?file=file1&file=file2
-//
-// Response: -
-//
-func deleteFile(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	filenames := r.Form["file"]
-	if len(filenames) == 0 {
-		http.Error(w, "list of files for deleting can't be empty", http.StatusBadRequest)
-		return
-	}
-
-	respChan := make(chan multiplyResponse, 50)
-	wg := new(sync.WaitGroup)
-
-	for _, filename := range filenames {
-		wg.Add(1)
-		go func(f string) {
-			defer wg.Done()
-			err := files.DeleteFile(f)
-			if err != nil {
-				respChan <- multiplyResponse{Filename: f, IsError: true, Error: err.Error()}
-			} else {
-				respChan <- multiplyResponse{Filename: f, Status: "deleted"}
-			}
-		}(filename)
-	}
-
-	wg.Wait()
-	close(respChan)
-
-	var response []multiplyResponse
-	for resp := range respChan {
-		response = append(response, resp)
-	}
-
-	enc := json.NewEncoder(w)
-	if params.Debug {
-		enc.SetIndent("", "  ")
-	}
-	enc.Encode(response)
-}
-
-// PUT /api/files?file=123&new-name=567&tags=tag1,tag2,tag3
-// newname or tags can be skipped
-//
-// Response: -
-//
-func changeFile(w http.ResponseWriter, r *http.Request) {
-	var (
-		filename = r.FormValue("file")
-		newName  = r.FormValue("new-name")
-		tags     = func() []string {
-			t := r.FormValue("tags")
-			if t == "" {
-				return []string{}
-			}
-
-			return strings.Split(t, ",")
-		}()
-	)
-
-	if filename == "" {
-		http.Error(w, ErrEmptyFilename.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// Change tags, at first
-	if len(tags) != 0 {
-		err := files.ChangeTags(filename, tags)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
-
-	// Change filename
-	if newName != "" {
-		// We can skip checking of invalid characters, because Go will return an error
-		err := files.RenameFile(filename, newName)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
-}
-
 // GET /api/files?sort=(name|size|time)&order(asc|desc)&tags=first,second,third&mode=(or|and|not)&search=abc
 // tags - list of tags separated by ',' (can be empty, then all files will be returned)
 // First elements in params is default (name, asc and etc.)
@@ -265,4 +130,137 @@ func returnRecentFiles(w http.ResponseWriter, r *http.Request) {
 		enc.SetIndent("", "  ")
 	}
 	enc.Encode(files)
+}
+
+// POST /api/files (multipart/form-data)
+//
+// Response: json list of strings with status of files uploading
+//
+func upload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(maxSize)
+	if err != nil {
+		switch err {
+		case http.ErrNotMultipart:
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	respChan := make(chan multiplyResponse, 50)
+	wg := new(sync.WaitGroup)
+
+	for _, f := range r.MultipartForm.File["files"] {
+		wg.Add(1)
+		go func(header *multipart.FileHeader) {
+			defer wg.Done()
+			err := files.UploadFile(header)
+			if err != nil {
+				respChan <- multiplyResponse{Filename: header.Filename, IsError: true, Error: err.Error()}
+			} else {
+				respChan <- multiplyResponse{Filename: header.Filename, Status: "uploaded"}
+			}
+		}(f)
+	}
+	wg.Wait()
+	close(respChan)
+
+	var response []multiplyResponse
+	for resp := range respChan {
+		response = append(response, resp)
+	}
+
+	enc := json.NewEncoder(w)
+	if params.Debug {
+		enc.SetIndent("", "  ")
+	}
+	enc.Encode(response)
+}
+
+// PUT /api/files?file=123&new-name=567&tags=tag1,tag2,tag3
+// newname or tags can be skipped
+//
+// Response: -
+//
+func changeFile(w http.ResponseWriter, r *http.Request) {
+	var (
+		filename = r.FormValue("file")
+		newName  = r.FormValue("new-name")
+		tags     = func() []string {
+			t := r.FormValue("tags")
+			if t == "" {
+				return []string{}
+			}
+
+			return strings.Split(t, ",")
+		}()
+	)
+
+	if filename == "" {
+		http.Error(w, ErrEmptyFilename.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Change tags, at first
+	if len(tags) != 0 {
+		err := files.ChangeTags(filename, tags)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// Change filename
+	if newName != "" {
+		// We can skip checking of invalid characters, because Go will return an error
+		err := files.RenameFile(filename, newName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+// DELETE /api/files?file=file1&file=file2
+//
+// Response: -
+//
+func deleteFile(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filenames := r.Form["file"]
+	if len(filenames) == 0 {
+		http.Error(w, "list of files for deleting can't be empty", http.StatusBadRequest)
+		return
+	}
+
+	respChan := make(chan multiplyResponse, 50)
+	wg := new(sync.WaitGroup)
+
+	for _, filename := range filenames {
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			err := files.DeleteFile(f)
+			if err != nil {
+				respChan <- multiplyResponse{Filename: f, IsError: true, Error: err.Error()}
+			} else {
+				respChan <- multiplyResponse{Filename: f, Status: "deleted"}
+			}
+		}(filename)
+	}
+
+	wg.Wait()
+	close(respChan)
+
+	var response []multiplyResponse
+	for resp := range respChan {
+		response = append(response, resp)
+	}
+
+	enc := json.NewEncoder(w)
+	if params.Debug {
+		enc.SetIndent("", "  ")
+	}
+	enc.Encode(response)
 }
