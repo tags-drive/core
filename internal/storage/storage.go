7440806b955b841e572391ea3536b292c9936efd
Shoshin Nikita <shoshin.nikita@gmail.com>
2018-11-25 13:08:33 +0300

    Add interfaces into storage package

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,9 +1,46 @@
 package storage
 
 import (
+	"io"
+	"mime/multipart"
+
 	"github.com/tags-drive/core/internal/storage/files"
 	"github.com/tags-drive/core/internal/storage/tags"
 )
+
+// FileStorageInterface provides methods for interactions with files
+type FileStorageInterface interface {
+	Init() error
+	//
+	Get(expr string, s files.SortMode, search string) []files.FileInfo
+	GetRecent(number int) []files.FileInfo
+	Archive() (io.Reader, error)
+	//
+	Upload(*multipart.FileHeader) error
+	//
+	Rename(oldName, newName string) error
+	ChangeTags(filename, tags []int) error
+	ChangeDescription(filename, newDescription string) error
+	//
+	Delete(filename string) error
+	DeleteForce(filename string) error
+	Recover(filename string)
+	DeleteTagFromFiles(tagID int)
+}
+
+var Files FileStorageInterface
+
+// TagStorageInterface provides methods for interactions with tags
+type TagStorageInterface interface {
+	Init() error
+	//
+	GetAll() tags.Tags
+	Add(tags.Tag)
+	Change(id int, newName, newColor string)
+	Delete(id int)
+}
+
+var Tags TagStorageInterface
 
 // Init calls files.Init() and tags.Init()
 func Init() error {
