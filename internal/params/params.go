51f06adc9c5bccd260fe62f1702005cbc7c9e46f
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-01-08 01:37:01 +0300

    [REF] move const vars into separate file

--- internal/params/params.go
+++ internal/params/params.go
@@ -6,26 +6,6 @@ import (
 	"errors"
 	"os"
 	"strings"
-	"time"
-)
-
-const (
-	// DataFolder is a folder where all files are kept
-	DataFolder = "data"
-	// ResizedImagesFolder is a folder where all resized images are kept
-	ResizedImagesFolder = "data/resized"
-	// Files is a json file with files information
-	Files = "configs/files.json"
-	// TokensFile is a json file with list of tokens
-	TokensFile = "configs/tokens.json"
-	// TagsFile is a json file with list of tags (with name and color)
-	TagsFile = "configs/tags.json"
-	// MaxTokenLife defines the max lifetime of a token (2 months)
-	MaxTokenLife = time.Hour * 24 * 60
-	// AuthCookieName defines name of cookie that contains token
-	AuthCookieName = "auth"
-	// JSONStorage is used for StorageType
-	JSONStorage = "json"
 )
 
 var (
