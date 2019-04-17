ce09895803029ae1c70cfd171decf7cdbd047041
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-04-17 17:03:42 +0300

    [WIP] refactor cmd/tags-drive/main.go
    * add App struct
    * add commonOptions struct
    * use github.com/kelseyhightower/envconfig to parse Env Vars
    * add MAX_TOKEN_LIFE env

--- cmd/tags-drive/main.go
+++ cmd/tags-drive/main.go
@@ -1,64 +1,132 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-	stdlog "log"
+	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
 	clog "github.com/ShoshinNikita/log/v2"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
 
-	"github.com/tags-drive/core/internal/params"
 	"github.com/tags-drive/core/internal/storage/files"
 	"github.com/tags-drive/core/internal/storage/tags"
 	"github.com/tags-drive/core/internal/web"
 )
 
-type App struct {
-	Server      web.ServerInterface
-	FileStorage files.FileStorageInterface
-	TagStorage  tags.TagStorageInterface
-	Logger      *clog.Logger
+const version = "v0.6.0"
+
+type commonOptions struct {
+	Version string `ignored:"true"`
+
+	// Debug defines is debug mode
+	Debug bool `envconfig:"DBG" default:"false"`
+
+	// Port of the server
+	Port string `envconfig:"PORT" default:":80"`
+	// IsTLS defines should the program use https
+	IsTLS bool `envconfig:"TLS" default:"false"`
+	// Login is a user login
+	Login string `envconfig:"LOGIN" default:"user"`
+	// Password is a user password
+	Password string `envconfig:"PSWRD" default:"qwerty"`
+	// SkipLogin let use Tags Drive without loginning (for Debug only)
+	SkipLogin bool `envconfig:"SKIP_LOGIN" default:"false"`
+	// MaxTokenLife defines the max lifetime of a token (2 months)
+	MaxTokenLife time.Duration `envconfig:"MAX_TOKEN_LIFE" default:"1440h"`
+	// AuthCookieName defines name of cookie that contains token
+	AuthCookieName string `default:"auth"`
+
+	// Encrypt defines, should the program encrypt files. False by default
+	Encrypt bool `envconfig:"ENCRYPT" default:"false"`
+	// PassPhrase is used to encrypt files. Key is a sha256 sum of env "PASS_PHRASE"
+	PassPhrase [32]byte `ignored:"true"`
+
+	// StorageType is a type of storage
+	StorageType string `envconfig:"STORAGE_TYPE" default:"json"`
+
+	// DataFolder is a folder where all files are kept
+	DataFolder string `default:"./data"`
+	// ResizedImagesFolder is a folder where all resized images are kept
+	ResizedImagesFolder string `default:"./data/resized"`
+
+	// FilesJSONFile is a json file with files information
+	FilesJSONFile string `default:"./configs/files.json"`
+	// TagsJSONFile is a json file with list of tags (with name and color)
+	TagsJSONFile string `default:"./configs/tags.json"`
+	// TokensJSONFile is a json file with list of tokens
+	TokensJSONFile string `default:"./configs/tokens.json"`
 }
 
-func main() {
-	if err := params.Parse(); err != nil {
-		stdlog.Fatalln(err)
+type App struct {
+	server      web.ServerInterface
+	fileStorage files.FileStorageInterface
+	tagStorage  tags.TagStorageInterface
+
+	logger *clog.Logger
+
+	options commonOptions
+}
+
+func NewApp() (*App, error) {
+	var opts commonOptions
+	err := envconfig.Process("", &opts)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't parse Config")
 	}
 
+	opts.Version = version
+
+	// Add PassPhrase
+	if opts.Encrypt {
+		phrase := os.Getenv("PASS_PHRASE")
+		if phrase == "" {
+			return nil, errors.New("wrong env config: PASS_PHRASE can't be empty with ENCRYPT=true")
+		}
+		opts.PassPhrase = sha256.Sum256([]byte(phrase))
+	}
+
+	app := &App{options: opts}
+
+	return app, nil
+}
+
+func (app *App) Start() error {
 	lg := clog.NewProdLogger()
-	if params.Debug {
+	if app.options.Debug {
 		lg = clog.NewDevLogger()
 	}
 
-	lg.Printf("Tags Drive %s (https://github.com/tags-drive)\n\n", params.Version)
+	lg.Printf("Tags Drive %s (https://github.com/tags-drive)\n\n", app.options.Version)
 
 	lg.Infoln("start")
 
-	// Print params
-	lg.Infoln("params:")
-	lg.Println(paramsToString())
+	// Print options
+	lg.Infoln("options:")
+	lg.Println(app.paramsToString())
 
 	var err error
 
-	app := new(App)
-	app.Logger = lg
+	app.logger = lg
 
-	app.FileStorage, err = files.NewFileStorage(lg)
+	app.fileStorage, err = files.NewFileStorage(lg)
 	if err != nil {
-		lg.Fatalf("can't create new FileStorage: %s\n", err)
+		return errors.Wrap(err, "can't create new FileStorage")
 	}
 
-	app.TagStorage, err = tags.NewTagStorage(lg)
+	app.tagStorage, err = tags.NewTagStorage(lg)
 	if err != nil {
-		lg.Fatalf("can't create new TagStorage: %s\n", err)
+		return errors.Wrap(err, "can't create new TagStorage")
 	}
 
-	app.Server, err = web.NewWebServer(app.FileStorage, app.TagStorage, lg)
+	app.server, err = web.NewWebServer(app.fileStorage, app.tagStorage, lg)
 	if err != nil {
-		lg.Fatalf("can't init WebServer: %s", err)
+		return errors.Wrap(err, "can't init WebServer")
 	}
 
 	shutdowned := make(chan struct{})
@@ -79,19 +147,19 @@ func main() {
 
 		// Shutdowns. Server must be first
 		lg.Infoln("shutdown WebServer")
-		err := app.Server.Shutdown()
+		err := app.server.Shutdown()
 		if err != nil {
 			lg.Warnf("can't shutdown server gracefully: %s\n", err)
 		}
 
 		lg.Infoln("shutdown FileStorage")
-		err = app.FileStorage.Shutdown()
+		err = app.fileStorage.Shutdown()
 		if err != nil {
 			lg.Warnf("can't shutdown FileStorage gracefully: %s\n", err)
 		}
 
 		lg.Infoln("shutdown TagStorage")
-		err = app.TagStorage.Shutdown()
+		err = app.tagStorage.Shutdown()
 		if err != nil {
 			lg.Warnf("can't shutdown TagStorage gracefully: %s\n", err)
 		}
@@ -99,7 +167,7 @@ func main() {
 		close(shutdowned)
 	}()
 
-	err = app.Server.Start()
+	err = app.server.Start()
 	if err != nil {
 		lg.Errorf("server error: %s\n", err)
 		close(fatalServerErr)
@@ -108,23 +176,25 @@ func main() {
 	<-shutdowned
 
 	lg.Infoln("stop")
+
+	return nil
 }
 
-func paramsToString() string {
+func (app *App) paramsToString() string {
 	s := "\n"
 
 	vars := []struct {
 		name string
 		v    interface{}
 	}{
-		{"Port", params.Port},
-		{"Login", params.Login},
-		{"Password", strings.Repeat("*", len(params.Password))},
-		{"TLS", params.IsTLS},
-		{"Encrypt", params.Encrypt},
-		{"StorageType", params.StorageType},
-		{"Debug", params.Debug},
-		{"SkipLogin", params.SkipLogin},
+		{"Port", app.options.Port},
+		{"Login", app.options.Login},
+		{"Password", strings.Repeat("*", len(app.options.Password))},
+		{"TLS", app.options.IsTLS},
+		{"Encrypt", app.options.Encrypt},
+		{"StorageType", app.options.StorageType},
+		{"Debug", app.options.Debug},
+		{"SkipLogin", app.options.SkipLogin},
 	}
 
 	for _, v := range vars {
@@ -133,4 +203,15 @@ func paramsToString() string {
 
 	// Remove the last '\n'
 	return s[:len(s)-1]
+}
+
+func main() {
+	app, err := NewApp()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := app.Start(); err != nil {
+		log.Fatalln(err)
+	}
 }
