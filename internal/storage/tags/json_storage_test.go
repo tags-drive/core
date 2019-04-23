06af328c438f21642ac6709f80898ef875d193d3
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-04-23 17:29:34 +0300

    [WIP] update tests #67

--- internal/storage/tags/json_storage_test.go
+++ internal/storage/tags/json_storage_test.go
@@ -1,247 +1,290 @@
 package tags
 
 import (
-	"bytes"
-	"io/ioutil"
 	"os"
-	"sync"
 	"testing"
 
 	clog "github.com/ShoshinNikita/log/v2"
+	"github.com/stretchr/testify/assert"
 )
 
-func areTagsEqual(a, b Tags) bool {
-	if len(a) != len(b) {
-		return false
-	}
+const testFile = "./tags.json"
 
-	for k, t := range a {
-		if tt, ok := b[k]; !ok || t != tt {
-			return false
-		}
-	}
-
-	return true
-}
-
-func removeConfigFile(path string) {
-	os.Remove(path)
-}
-
-func newStorage() *jsonTagStorage {
+func newReadyStorage() (*jsonTagStorage, error) {
 	cnf := Config{
 		Debug:        false,
 		StorageType:  "json",
-		TagsJSONFile: "tags.json",
+		TagsJSONFile: testFile,
 		Encrypt:      false,
 		// PassPhrase:   sha256.Sum256([]byte("sha256")),
 	}
-	return newJsonTagStorage(cnf, clog.NewProdLogger())
+
+	st := newJsonTagStorage(cnf, clog.NewProdLogger())
+	err := st.init()
+
+	return st, err
 }
 
 func TestInit(t *testing.T) {
-	testStorage := newStorage()
-	testStorage.init()
+	assert := assert.New(t)
+	cnf := Config{
+		Debug:        false,
+		StorageType:  "json",
+		TagsJSONFile: testFile,
+		Encrypt:      false,
+		// PassPhrase:   sha256.Sum256([]byte("sha256")),
+	}
+
+	testStorage := newJsonTagStorage(cnf, clog.NewProdLogger())
 
 	err := testStorage.init()
-	if err != nil {
-		t.Fatal(err)
+	if !assert.Nil(err, "can't init test storage") {
+		t.FailNow()
 	}
 
-	f, err := os.Open(testStorage.config.TagsJSONFile)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer f.Close()
+	err = testStorage.shutdown()
+	assert.Nil(err, "can't init test storage")
 
-	data, err := ioutil.ReadAll(f)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !(bytes.Equal(data, []byte("{}")) || bytes.Equal(data, []byte("{}\n"))) {
-		t.Errorf("Wrong file content: %s", string(data))
-	}
-
-	removeConfigFile(testStorage.config.TagsJSONFile)
+	os.Remove(testFile)
 }
 
-func TestAdd(t *testing.T) {
-	testStorage := newStorage()
-	testStorage.init()
+func TestAddAndDelete(t *testing.T) {
+	type testType int
+	const (
+		add testType = iota
+		delete
+	)
 
-	tags := []Tag{
-		{Name: "test1", Color: "#fffff0"},
-		{Name: "test2", Color: "#ffff0f"},
-		{Name: "test3", Color: "#fff0ff"},
-		{Name: "test4", Color: "#ff0fff"},
-		{Name: "test5", Color: "#f0ffff"},
-		{Name: "test6", Color: "#0fffff"},
-	}
+	assert := assert.New(t)
 
-	answer := Tags{
-		1: Tag{ID: 1, Name: "test1", Color: "#fffff0"},
-		2: Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
-		3: Tag{ID: 3, Name: "test3", Color: "#fff0ff"},
-		4: Tag{ID: 4, Name: "test4", Color: "#ff0fff"},
-		5: Tag{ID: 5, Name: "test5", Color: "#f0ffff"},
-		6: Tag{ID: 6, Name: "test6", Color: "#0fffff"},
-	}
-
-	for _, tag := range tags {
-		testStorage.addTag(tag)
-	}
-
-	result := testStorage.getAll()
-
-	if !areTagsEqual(result, answer) {
-		t.Errorf("Want: %v\n\nGot: %v", answer, result)
-	}
-
-	removeConfigFile(testStorage.config.TagsJSONFile)
-}
-
-func TestDelete(t *testing.T) {
-	testStorage := newStorage()
-	testStorage.init()
-
-	// Default tags
-	startTags := []Tag{
-		{Name: "test1", Color: "#fffff0"},
-		{Name: "test2", Color: "#ffff0f"},
-		{Name: "test3", Color: "#fff0ff"},
-		{Name: "test4", Color: "#ff0fff"},
-		{Name: "test5", Color: "#f0ffff"},
-		{Name: "test6", Color: "#0fffff"},
-	}
-	for _, tag := range startTags {
-		testStorage.addTag(tag)
-	}
-
-	idsToDelete := []int{1, 3, 5, 10}
-	for _, id := range idsToDelete {
-		testStorage.deleteTag(id)
-	}
-
-	// Check
-	answer := Tags{
-		2: Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
-		4: Tag{ID: 4, Name: "test4", Color: "#ff0fff"},
-		6: Tag{ID: 6, Name: "test6", Color: "#0fffff"},
-	}
-	result := testStorage.getAll()
-	if !areTagsEqual(result, answer) {
-		t.Errorf("Want: %v\n\nGot: %v", answer, result)
-	}
-
-	// Add new Tags
-	newTags := []Tag{
-		{Name: "123", Color: "#ff0000"},
-		{Name: "456", Color: "#00ff00"},
-		{Name: "789", Color: "#0000ff"},
-	}
-	for _, tag := range newTags {
-		testStorage.addTag(tag)
-	}
-
-	answer = Tags{
-		2: Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
-		4: Tag{ID: 4, Name: "test4", Color: "#ff0fff"},
-		6: Tag{ID: 6, Name: "test6", Color: "#0fffff"},
-		7: Tag{ID: 7, Name: "123", Color: "#ff0000"},
-		8: Tag{ID: 8, Name: "456", Color: "#00ff00"},
-		9: Tag{ID: 9, Name: "789", Color: "#0000ff"},
-	}
-	result = testStorage.getAll()
-	if !areTagsEqual(result, answer) {
-		t.Errorf("Want: %v\n\nGot: %v", answer, result)
-	}
-
-	removeConfigFile(testStorage.config.TagsJSONFile)
-}
-
-func TestUpdate(t *testing.T) {
-	startTags := []Tag{
-		{Name: "test1", Color: "#fffff0"},
-		{Name: "test2", Color: "#ffff0f"},
-		{Name: "test3", Color: "#fff0ff"},
-		{Name: "test4", Color: "#ff0fff"},
-		{Name: "test5", Color: "#f0ffff"},
-		{Name: "test6", Color: "#0fffff"},
-	}
-
-	type toUpdate struct {
-		id    int
-		name  string
-		color string
+	storage, err := newReadyStorage()
+	if !assert.Nil(err, "can't init storage") {
+		t.FailNow()
 	}
 
 	tests := []struct {
-		update toUpdate
-		answer Tag
-		ok     bool
+		testType     testType
+		tagsToAdd    []Tag // if testType == add
+		tagsToDelete []int // if testType == delete
+		result       Tags
 	}{
-		// No changes
 		{
-			toUpdate{id: 1, name: "", color: ""},
-			Tag{ID: 1, Name: "test1", Color: "#fffff0"},
-			true,
+			testType: add,
+			tagsToAdd: []Tag{
+				{Name: "test1", Color: "#fffff0"},
+			},
+			result: Tags{
+				1: Tag{ID: 1, Name: "test1", Color: "#fffff0"},
+			},
 		},
-		// Change name
 		{
-			toUpdate{id: 5, name: "hello", color: ""},
-			Tag{ID: 5, Name: "hello", Color: "#f0ffff"},
-			true,
+			testType: add,
+			tagsToAdd: []Tag{
+				{Name: "test2", Color: "#ffff0f"},
+				{Name: "test3", Color: "#fff0ff"},
+			},
+			result: Tags{
+				1: Tag{ID: 1, Name: "test1", Color: "#fffff0"},
+				2: Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
+				3: Tag{ID: 3, Name: "test3", Color: "#fff0ff"},
+			},
 		},
-		// Change color (without #)
 		{
-			toUpdate{id: 4, name: "", color: "ff0000"},
-			Tag{ID: 4, Name: "test4", Color: "#ff0000"},
-			true,
+			testType: add,
+			tagsToAdd: []Tag{
+				{Name: "test4", Color: "#ff0fff"},
+				{Name: "test5", Color: "#f0ffff"},
+				{Name: "test6", Color: "#0fffff"},
+			},
+			result: Tags{
+				1: Tag{ID: 1, Name: "test1", Color: "#fffff0"},
+				2: Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
+				3: Tag{ID: 3, Name: "test3", Color: "#fff0ff"},
+				4: Tag{ID: 4, Name: "test4", Color: "#ff0fff"},
+				5: Tag{ID: 5, Name: "test5", Color: "#f0ffff"},
+				6: Tag{ID: 6, Name: "test6", Color: "#0fffff"},
+			},
 		},
-		// Change name and color
 		{
-			toUpdate{id: 2, name: "123", color: "#efefef"},
-			Tag{ID: 2, Name: "123", Color: "#efefef"},
-			true,
+			testType: add,
+			tagsToAdd: []Tag{
+				{Name: "test6", Color: "#111111"},
+			},
+			result: Tags{
+				1: Tag{ID: 1, Name: "test1", Color: "#fffff0"},
+				2: Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
+				3: Tag{ID: 3, Name: "test3", Color: "#fff0ff"},
+				4: Tag{ID: 4, Name: "test4", Color: "#ff0fff"},
+				5: Tag{ID: 5, Name: "test5", Color: "#f0ffff"},
+				6: Tag{ID: 6, Name: "test6", Color: "#0fffff"},
+				7: Tag{ID: 7, Name: "test6", Color: "#111111"},
+			},
 		},
-		// Wrong id
 		{
-			toUpdate{id: 89, name: "123", color: "#efefef"},
-			Tag{},
-			false,
+			testType: delete,
+			tagsToDelete: []int{
+				1,
+				3,
+				5,
+			},
+			result: Tags{
+				2: Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
+				4: Tag{ID: 4, Name: "test4", Color: "#ff0fff"},
+				6: Tag{ID: 6, Name: "test6", Color: "#0fffff"},
+				7: Tag{ID: 7, Name: "test6", Color: "#111111"},
+			},
 		},
-	}
+		{
+			testType: add,
+			tagsToAdd: []Tag{
+				{Name: "new tag", Color: "#111111"},
+				{Name: "adsbcv", Color: "#222222"},
+				{Name: "scvxcv", Color: "#111111"},
+			},
+			result: Tags{
+				2:  Tag{ID: 2, Name: "test2", Color: "#ffff0f"},
+				4:  Tag{ID: 4, Name: "test4", Color: "#ff0fff"},
+				6:  Tag{ID: 6, Name: "test6", Color: "#0fffff"},
+				7:  Tag{ID: 7, Name: "test6", Color: "#111111"},
+				8:  Tag{ID: 8, Name: "new tag", Color: "#111111"},
+				9:  Tag{ID: 9, Name: "adsbcv", Color: "#222222"},
+				10: Tag{ID: 10, Name: "scvxcv", Color: "#111111"},
+			},
+		},
+		{
+			testType: delete,
+			tagsToDelete: []int{
+				2,
+				4,
+				6,
+				7,
+				9,
+			},
+			result: Tags{
 
-	testStorage := newStorage()
-	testStorage.init()
-
-	wg := new(sync.WaitGroup)
-	for _, tag := range startTags {
-		testStorage.addTag(tag)
+				8:  Tag{ID: 8, Name: "new tag", Color: "#111111"},
+				10: Tag{ID: 10, Name: "scvxcv", Color: "#111111"},
+			},
+		},
 	}
 
 	for i, tt := range tests {
-		wg.Add(1)
-
-		go func(testID int, up toUpdate, ansTag Tag, ansOk bool) {
-			defer wg.Done()
-
-			testStorage.updateTag(up.id, up.name, up.color)
-			result := testStorage.getAll()
-			tag, ok := result[ansTag.ID]
-			if ok != ansOk {
-				t.Errorf("Test #%d wrong ok. Want: %v Got: %v", testID, ansOk, ok)
-				return
+		if tt.testType == add {
+			for _, tag := range tt.tagsToAdd {
+				storage.addTag(tag)
 			}
-
-			if tag != ansTag {
-				t.Errorf("Test #%d. Want: %v Got: %v", testID, ansTag, tag)
+		} else if tt.testType == delete {
+			for _, id := range tt.tagsToDelete {
+				storage.deleteTag(id)
 			}
-		}(i, tt.update, tt.answer, tt.ok)
+		}
+
+		res := storage.getAll()
+		assert.Equalf(tt.result, res, "iteration #%d", i)
 	}
 
-	wg.Wait()
+	storage.shutdown()
+	os.Remove(testFile)
+}
 
-	removeConfigFile(testStorage.config.TagsJSONFile)
+func TestUpdate(t *testing.T) {
+	type testType int
+	const (
+		updateTag testType = iota
+		updateGroup
+	)
+
+	assert := assert.New(t)
+
+	storage, err := newReadyStorage()
+	if !assert.Nil(err, "can't init storage") {
+		t.FailNow()
+	}
+
+	startTags := []Tag{
+		{Name: "test1", Color: "#fffff0"},
+		{Name: "test2", Color: "#ffff0f"},
+		{Name: "test3", Color: "#fff0ff"},
+		{Name: "test4", Color: "#ff0fff"},
+		{Name: "test5", Color: "#f0ffff"},
+		{Name: "test6", Color: "#0fffff"},
+	}
+
+	tests := []struct {
+		testType testType
+		id       int
+		newName  string
+		newColor string
+		newGroup string
+		result   Tag
+	}{
+		// No changes
+		{
+			testType: updateTag,
+			id:       1,
+			newName:  "",
+			newColor: "",
+			result:   Tag{ID: 1, Name: "test1", Color: "#fffff0"},
+		},
+		// Change name
+		{
+			testType: updateTag,
+			id:       5,
+			newName:  "hello",
+			newColor: "",
+			result:   Tag{ID: 5, Name: "hello", Color: "#f0ffff"},
+		},
+		// Change color (without #)
+		{
+			testType: updateTag,
+			id:       4,
+			newName:  "",
+			newColor: "ff0000",
+			result:   Tag{ID: 4, Name: "test4", Color: "#ff0000"},
+		},
+		// Change name and color
+		{
+			testType: updateTag,
+			id:       2,
+			newName:  "123",
+			newColor: "#efefef",
+			result:   Tag{ID: 2, Name: "123", Color: "#efefef"},
+		},
+		// Change group
+		{
+			testType: updateGroup,
+			id:       2,
+			newGroup: "test",
+			result:   Tag{ID: 2, Name: "123", Color: "#efefef", Group: "test"},
+		},
+		// Change group (to "")
+		{
+			testType: updateGroup,
+			id:       2,
+			newGroup: "",
+			result:   Tag{ID: 2, Name: "123", Color: "#efefef", Group: ""},
+		},
+	}
+
+	// Add start tags
+	for _, t := range startTags {
+		storage.addTag(t)
+	}
+
+	var res Tag
+	for i, tt := range tests {
+		if tt.testType == updateTag {
+			res, err = storage.updateTag(tt.id, tt.newName, tt.newColor)
+		} else if tt.testType == updateGroup {
+			res, err = storage.updateGroup(tt.id, tt.newGroup)
+		}
+
+		if !assert.Nilf(err, "iteration %d: got an error: %s", i, err) {
+			continue
+		}
+		assert.Equalf(tt.result, res, "iteration %d", i)
+	}
+
+	storage.shutdown()
+	os.Remove(testFile)
 }
