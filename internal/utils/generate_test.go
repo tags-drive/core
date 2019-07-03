ec71eb1ac9bfbed81ef2f3214684408fdba7ad0e
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-07-03 13:42:30 +0300

    [FIX] utils: fix "BenchmarkGenerate"

--- internal/utils/generate_test.go
+++ internal/utils/generate_test.go
@@ -1,8 +1,6 @@
 package utils_test
 
 import (
-	"fmt"
-	"io/ioutil"
 	"testing"
 
 	"github.com/tags-drive/core/internal/utils"
@@ -12,6 +10,6 @@ func BenchmarkGenerate(b *testing.B) {
 	const maxSize = 16
 
 	for i := 0; i < b.N; i++ {
-		fmt.Fprint(ioutil.Discard, utils.GenerateRandomString(maxSize))
+		_ = utils.GenerateRandomString(maxSize)
 	}
 }
