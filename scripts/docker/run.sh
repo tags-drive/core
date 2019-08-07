8ecb6b4a23867dd5a9b809850588c3a995d7b255
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-08-08 01:24:42 +0400

    [FIX] scripts: update env vars

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-export PORT=80
-export TLS=false
-export LOGIN=user
-export PSWRD=qwerty
-export ENCRYPT=false
-export SKIP_LOGIN=true
-export DBG=true
+export DEBUG=true
+export WEB_PORT=:80
+export WEB_TLS=false
+export WEB_LOGIN=user
+export WEB_PASSWORD=qwerty
+export WEB_SKIP_LOGIN=true
+export STORAGE_ENCRYPT=false
 
 ./tags-drive
