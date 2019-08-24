412d02df6cf766abb1b96a6afaa0ad018938e6d6
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-08-25 02:09:03 +0300

    [UPD] scripts/run: update env vars
    * Update setupEnv: skip commented lines
    * Update "run.env"

--- scripts/run/run.py
+++ scripts/run/run.py
@@ -3,6 +3,15 @@
 import os
 
 
+def isEmpty(s: str) -> bool:
+    if len(s) == 0:
+        return True
+    if len(s) == 1 and s[0] == "\n":
+        return True
+
+    return False
+
+
 def setupEnv(file: str):
     try:
         f = open(file, "r")
@@ -11,6 +20,13 @@ def setupEnv(file: str):
         exit(1)
 
     for line in f.readlines():
+        if isEmpty(line):
+            continue
+
+        if line[0] == "#":
+            # Skip commented lines
+            continue
+
         if line[-1] == "\n":
             # Trim \n
             line = line[:len(line)-1]
